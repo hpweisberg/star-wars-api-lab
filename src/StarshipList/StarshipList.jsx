6dedd7ef0b5148a718e1cfd44c9a8e6a3648023d
hpweisberg <hpweisberg@gmail.com>
2023-01-26 15:54:36 -0800

    add StarshipList as landing page

---
+++ src/StarshipList/StarshipList.jsx
@@ -0,0 +1,17 @@
+// import useState:
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// Services:
+import { getAllStarships } from "../services/sw-api";
+
+
+const StarshipList = () => {
+  return ( 
+    <>
+      <h3>test</h3>
+    </>
+  )
+}
+
+export default StarshipList
